e92ea8a68fbe98b580ce29f322f4d1f1fe361de0
Henry Adams <henrybranchadams@gmail.com>
2026-09-03 18:14:58 -0500

    polish: load the final visual-system layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'tldraw/tldraw.css'
 import './styles.css'
+import './visual-polish.css'
 import App from './app/App'
 
 const root = document.getElementById('root')
